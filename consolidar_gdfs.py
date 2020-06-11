bb31eeec1d779ffc13d00eea46baa073403fdd18
alephcero <felipegonz@gmail.com>
2020-06-11 09:11:29 -0300

    primer commit

---
+++ consolidar_gdfs.py
@@ -0,0 +1,19 @@
+import geopandas as gpd
+import os
+
+partidos = os.listdir('carto')
+partidos
+
+edges = gpd.GeoDataFrame()
+nodes = gpd.GeoDataFrame()
+
+for partido in partidos:
+    ruta = 'carto/'+partido+'/'
+    node = gpd.read_file(ruta + 'nodes.shp')
+    edge = gpd.read_file(ruta + 'edges.shp')
+    nodes = nodes.append(node)
+    edges = edges.append(edge)
+
+
+edges.to_file('carto/region_edges.geojson',driver='GeoJSON')
+nodes.to_file('carto/region_nodes.geojson',driver='GeoJSON')
